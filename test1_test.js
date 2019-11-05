594b579ea1ae960de361a05ff29dba863df30fe6
LRaghawan <linesh.raghawan@difi.no>
2019-11-05 08:32:35 +0100

    Update test1_test.js

--- test1_test.js
+++ test1_test.js
@@ -2,13 +2,13 @@
 Feature('sp-web-login');
 
 Scenario('test something',  (I)   => {
-	I.amOnPage('https://sp-web-ver2.azurewebsites.net/');
-		 I.see('Samarbeidsportalen');
-		 I.click('Logg inn');
-		 I.fillField('username','utvtest@difi.no');
-		 I.fillField('password','Testadmin123');
-		 I.click('login');
-		 I.see('INTEGRASJONER');
-		 I.see('MINE API');
-		 I.see('TEST1');
+	I.amOnPage('https://sp-web-test-test1.azurewebsites.net/');
+	I.see('Samarbeidsportalen');
+	I.click('login-button');
+	I.fillField('username','utvtest@difi.no');
+	I.fillField('password','Testadmin123');
+	I.click('login');
+	I.see('INTEGRASJONER');
+	I.see('MINE API');
+	I.see('TEST1');
 });
