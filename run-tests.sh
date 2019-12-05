ae0706097bbd6cd81365534cd3deedf0bc6aa8b2
LRaghawan <linesh.raghawan@difi.no>
2019-12-05 09:51:57 +0100

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -15,5 +15,7 @@ mkdir -p output
 #install npm og andre avhenginger
 npm install
 npm run build
-npm install codeceptjs puppeteer mocha-junit-reporter randomstring --save-dev
-npx codeceptjs run --steps --verbose --reporter mocha-junit-reporter 
+#npm install codeceptjs puppeteer mocha-junit-reporter randomstring --save-dev
+#npx codeceptjs run --steps --verbose --reporter mocha-junit-reporter 
+npm install codeceptjs puppeteer mochawesome randomstring --save-dev
+npx codeceptjs run --steps --verbose --reporter mochawesome
