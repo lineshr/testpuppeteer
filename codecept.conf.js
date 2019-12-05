4e0c21ebc842922ba5cc9b323c8f38e85b0907dc
LRaghawan <linesh.raghawan@difi.no>
2019-12-05 09:54:12 +0100

    Update codecept.conf.js

--- codecept.conf.js
+++ codecept.conf.js
@@ -17,28 +17,16 @@ exports.config = {
   include: {
     I: './steps_file.js'
   },
-mocha: {
-        reporterOptions: {
-            'codeceptjs-cli-reporter': {
-                stdout: './output/result.xml',
-                options: {
-                    verbose: true,
-                    steps: true,
-                },
-            },
-            mochawesome: {
-                stdout: './output/console/console.log',
-                options: {
-                    reportDir: './output/mochawesome/',
-                    reportFilename: 'report',
-                    quiet: true,
-                    json: false,
-                    html: true,
-                    overwrite: true,
-                },
-            },
-        },
-    },
+  mocha: {
+    reporterOptions: {
+        reportDir: 'output'
+    }
+  },
+  helpers: {
+    Mochawesome: {
+        'uniqueScreenshotNames': 'true'
+    }
+  },
   bootstrap: null,
   name: 'webdriverpuppeteer'
 }
