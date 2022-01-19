b5d38d6ec7d855a382c2674f41175ba5f65441fc
LRaghawan <linesh.raghawan@digdir.no>
2022-01-19 11:57:53 +0100

    Update codecept.conf.js

--- codecept.conf.js
+++ codecept.conf.js
@@ -2,12 +2,24 @@ exports.config = {
   tests: './*_test.js',
   output: './output',
   helpers: {
-    Puppeteer: {
-      url :'https://sp-web-test-test1.azurewebsites.net/',
-      chrome:{
-        args: ['--no-sandbox']
-      }
-    }
+      WebDriver : {
+          smartWait: 5000,
+              browser: "chrome",
+              restart: false,
+              windowSize: "maximize",
+              show: true,
+                  url: "http://dummy.no",
+            desiredCapabilities: {
+                chromeOptions: {
+                    args: [ '--proxy-server=http://localhost:8090 ', '--disable-features=IsolateOrigins,site-per-process', '--disable-site-per-process', '--disable-web-security', '--no-sandbox','--allow-running-insecure-content','--ignore-certificate-errors']
+                }
+            },
+              timeouts: {
+            "script": 60000,
+                "page load": 10000
+          },
+
+        },
   },
   include: {
     I: './steps_file.js'
