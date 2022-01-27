0a99418709f173b26ee6a2ad243dcf96fc6b1501
LRaghawan <linesh.raghawan@digdir.no>
2022-01-27 21:25:36 +0100

    Update sp-web_innloggin_test.js

--- sp-web_innloggin_test.js
+++ sp-web_innloggin_test.js
@@ -5,12 +5,5 @@ Scenario('sp-web loginn',  (I)   => {
 	   I.amOnPage('https://sp-web-test-test1.azurewebsites.net/');
 		 I.see('Samarbeidsportalen');
 		 I.click('Logg inn');
-		 I.fillField('username',process.env.SP_WEB_USERNAME);
-		 I.fillField('password',process.env.SP_WEB_PASSWORD);
-		 I.click('login');
-		 I.see('INTEGRASJONER');
-		 I.dontSee("PROD");
-		 I.see('MINE API');
-		 I.see('TEST1');
 		 I.click('//a[@href="/auth/logout"]');
 });
