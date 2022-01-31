759eec383a35f3237e97e20a55dced184814a131
LRaghawan <linesh.raghawan@digdir.no>
2022-01-31 14:00:14 +0100

    Update sp-web_innloggin_test.js

--- sp-web_innloggin_test.js
+++ sp-web_innloggin_test.js
@@ -2,8 +2,11 @@
 Feature('sp-web-login');
 
 Scenario('sp-web loginn',  (I)   => {
-	   I.amOnPage('https://sp-web-test-test1.azurewebsites.net/');
-		 I.see('Samarbeidsportalen');
-		 I.click('Logg inn');
-		 I.click('//a[@href="/auth/logout"]');
+I.amOnPage('https://github.com');
+I.click('Sign in', '//html/body/div[1]/header');
+I.see('Sign in to GitHub', 'h1');
+I.fillField('Username or email address', 'something@totest.com');
+I.fillField('Password', '123456');
+I.click('Sign in');
+I.see('Incorrect username or password.', '.flash-error');
 });
