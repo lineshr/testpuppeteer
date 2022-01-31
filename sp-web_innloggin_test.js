a51faef831aebaeeadd99f782cecac31afbdd603
LRaghawan <linesh.raghawan@digdir.no>
2022-01-31 14:29:56 +0100

    Update sp-web_innloggin_test.js

--- sp-web_innloggin_test.js
+++ sp-web_innloggin_test.js
@@ -5,7 +5,13 @@ Feature('sp-web-login');
 Scenario('pålogging via api @ansattporten', async ({I}) => {
 await I.amOnPage('http://test-client-ansattporten-systest.dev.eid.digdirnfl.no/');
 await I.click('Login');
-await I.click('testid1');
+
+          await I.see('Vel elektronisk ID');
+    await I.see('Slik skaffar du deg elektronisk ID');
+    await I.see('Ansattporten let deg logga inn på vegne av ei verksemd.');
+    await I.see('Hjelp til innlogging');
+    await I.see('Tryggleik og personvern');
+    await I.click('testid');
 await I.click('Avbryt');
 
 });
