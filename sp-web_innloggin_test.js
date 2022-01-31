8deaa0eeebfe16254cfed4002ea42c090fa26db0
LRaghawan <linesh.raghawan@digdir.no>
2022-01-31 14:21:11 +0100

    Update sp-web_innloggin_test.js

--- sp-web_innloggin_test.js
+++ sp-web_innloggin_test.js
@@ -3,11 +3,9 @@ Feature('sp-web-login');
 
 
 Scenario('pålogging via api @ansattporten', async ({I}) => {
-I.amOnPage('https://github.com');
-I.click('Sign in', '//html/body/div[1]/header');
-I.see('Sign in to GitHub', 'h1');
-I.fillField('Username or email address', 'something@totest.com');
-I.fillField('Password', '123456');
-I.click('Sign in');
-I.see('Incorrect username or password.', '.flash-error');
+await I.amOnPage('http://test-client-ansattporten-systest.dev.eid.digdirnfl.no/');
+await I.click('Login');
+await I.click('testid1');
+await I.click('Avbryt');
+
 });
